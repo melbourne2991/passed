4061573ae04c54836db50e26074fa5d540394184
Will <wleach1992@gmail.com>
2014-05-29 20:31:24 +1000

    generators

---
+++ generators/generate.js
@@ -0,0 +1,13 @@
+var cli     		  = require('../lib/cli');
+var args    		  = process.argv.slice(2, process.argv.length);
+var command 		  = args[0];
+var type    		  = args[1];
+var gen_args    = args[2];
+
+if(!cli[command]) {
+	console.log(command + ' is not a valid command');
+} else if (!cli[command][type] && command === 'gen') {
+	console.log(type + ' is not a valid generator type.')
+} else {
+	cli[command][type](gen_args);
+}
